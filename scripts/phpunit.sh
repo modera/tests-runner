e7b3728a64b891c1d02fd380402e044080c367fa
Sergei Lissovski <sergei.lissovski@gmail.com>
2016-09-08 13:10:27 +0300

    [MPFE-927] foundation's original phpunit.sh now is located in scripts/

--- scripts/phpunit.sh
+++ scripts/phpunit.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-set -e
+# Installs vendors, tests-runner and eventually runs tests
+#
+# If you need to run tests often then you can use "--md" configuration parameter and script then will not
+# terminate MySQL container after the tests run, for example:
+# $ ./phpunit.sh --md
+# Importantly, "--md" argument must always be specified in a first because all arguments given afterwards are
+# passed to PHPUnit as is. For example, this will run tests of src/Foo/Bar directory:
+# $ ./phpunit.sh --md src/Foo/Bar
 
-if ! type docker > /dev/null; then
-    echo "Docker is required to run tests."
-    exit 1
-fi
+RUNNER_GIT_DIR="mtr"
+
+set -e
 
 args=$@
 is_daemon=false
@@ -14,6 +20,42 @@ if [[ ${args:0:4} == "--md" ]]; then
   args=${args:4}
   is_daemon=true
 fi
+
+if ! type docker > /dev/null; then
+    echo "Docker is required to run tests."
+    exit 1
+fi
+
+if [ ! -d "vendor" ]; then
+  echo "# No vendor dir detected, installing modera/composer-monorepo-plugin and then project's dependencies"
+
+  printf "composer global require modera/composer-monorepo-plugin:dev-master\ncomposer install" > install.sh
+  chmod +x install.sh
+
+  docker run \
+  -it \
+  --rm \
+  -v `pwd`:/mnt/tmp \
+  -w /mnt/tmp \
+  modera/php7-fpm "./install.sh"
+
+  rm install.sh
+fi
+
+if [ ! -d "$RUNNER_GIT_DIR" ]; then
+  echo "# Cloning and installing test-runner"
+
+  git clone https://github.com/modera/tests-runner.git $RUNNER_GIT_DIR
+
+  docker run \
+  -it \
+  --rm \
+  -v `pwd`/$RUNNER_GIT_DIR:/mnt/tmp \
+  -w /mnt/tmp \
+  modera/php7-fpm "composer install"
+fi
+
+# if there's no mtr_php image then create a Docker file in $RUNNER_GIT_DIR and build it
 
 if [[ `docker ps` != *"mtr_mysql"* ]]; then
   if [ "$is_daemon" = true ] ; then
